709ffb188a12caca00da73ab453f42a9c647a591
Ram Rachum <cool-rr@cool-rr.com>
2010-07-11 16:44:52 +0200

    -

--- amir_pdf_grabber.py
+++ amir_pdf_grabber.py
@@ -5,6 +5,8 @@ import urllib2
 
 import BeautifulSoup as beautiful_soup_module
 
+
+
 DEFAULT_SEMESTER = 'Spring2010'
 
 pdf_file_pattern = re.compile('^.*\.pdf$')
@@ -36,13 +38,23 @@ def get_pdf_list_links_from_soup(soup, base_url):
     a_tags = soup.findAll(
         'a',
         attrs={
-            'href': re.compile(r'^%s.*$' % base_url)
+            'href': re.compile(r'^%s.*$' % 'ho_')
         }
     )
-    pdf_list_urls = [
+    
+    pdf_list_pseudourls = [
         dict(a_tag.attrs)['href'] for a_tag in a_tags
     ]
-    return pdf_list_urls
+    
+    pdf_list_urls = [
+        pdf_list_pseudourl \
+        if pdf_list_pseudourl.startswith('http') else
+        base_url.rsplit('/', 1)[0] + '/' + pdf_list_pseudourl
+        
+        for pdf_list_pseudourl in pdf_list_pseudourls
+    ]    
+    
+    return pdf_list_urls 
 
 
 def download_urls_to_folder(urls, folder):
@@ -66,7 +78,7 @@ def download_pdf_links_to_folder(url, folder):
     
 def course_number_to_base_url(course_number, semester=DEFAULT_SEMESTER):
     return 'http://webcourse.cs.technion.ac.il/%s/%s/en/ho' % \
-           course_number, semester
+           (course_number, semester)
 
 
 def get_all_pdf_list_urls_from_base_url(base_url):
@@ -77,17 +89,73 @@ def get_all_pdf_list_urls_from_base_url(base_url):
     while uncrawled_pdf_list_urls:
         pdf_list_url = uncrawled_pdf_list_urls.pop()
         soup = get_soup_from_url(pdf_list_url)
-        for child_pdf_list_url in get_pdf_list_links_from_soup(soup):
-            pdf_list_url.add(child_pdf_list_url)
+        for child_pdf_list_url in get_pdf_list_links_from_soup(soup, base_url):
+            pdf_list_urls.add(child_pdf_list_url)
             uncrawled_pdf_list_urls.add(child_pdf_list_url)
     
     return pdf_list_urls
 
         
 def download_pdfs_recursively_for_course_number(course_number,
+                                                folder=None,
                                                 semester=DEFAULT_SEMESTER):
+    if folder is None:
+        folder = '.'
+    folder = os.path.join(folder, str(course_number))
+    if not os.path.isdir(folder):
+        os.mkdir(folder)
     base_url = course_number_to_base_url(course_number, semester)
     pdf_list_urls = get_all_pdf_list_urls_from_base_url(base_url)
     
-    
+    for pdf_list_url in pdf_list_urls:
         
+        file_name = get_file_name_from_url(pdf_list_url)
+        if file_name == 'ho.html':
+            little_folder = folder    
+        else:
+            title = file_name[3:].rsplit('.', 1)[0]
+            # Cutting off '_ho' and extenstion
+            little_folder = os.path.join(folder, title)
+            
+        download_pdf_links_to_folder(
+            pdf_list_url, 
+            little_folder
+        )
+   
+        
+HELP_STRING = \
+r'''
+Usage:
+
+    amir_pdf_grabber.py 122435
+    
+    This will download PDFs for course number 122435 to folder ./122435
+
+    
+To select a specific folder, do like this:
+
+    amir_pdf_grabber.py 122435 c:\Documents
+    
+    This will download PDFs for course number 122435 to folder c:\Documents\122435
+'''
+        
+if __name__ == '__main__':
+    
+    try:
+        course_number = int(sys.argv[1])
+    except ValueError:
+        if 'help' in sys.argv[1]:
+            print(HELP_STRING)
+            exit()
+        else:
+            raise Exception('%s is not a valid course number' % sys.argv[1])
+    except IndexError:
+        print(HELP_STRING)
+        exit()
+    try:
+        folder = sys.argv[2]
+    except IndexError:
+        folder = '.'
+    
+    download_pdfs_recursively_for_course_number(course_number, folder) 
+    
